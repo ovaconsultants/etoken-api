0590a81510958a4e137f304b2fbd905b330286be
ovaconsultants <hr@ova2consultancy.com>
2025-02-08 17:55:30 -0500

    .

---
+++ middleware/asyncHandler.js
@@ -0,0 +1,21 @@
+// Generic error-handling wrapper for async functions
+const logExceptionToDb = require("../utils/logExceptionToDb");
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+      //logExceptionToDb("request completed successfully");
+      await fn(req, res, next);
+     
+    } catch (error) {
+      console.error("Error:", error.message);
+      await logExceptionToDb(error.message);
+  
+      res.status(500).json({
+        success: false,
+        error: "Internal Server Error",
+        message: error.message
+      });
+    }
+  };
+  
+  module.exports = asyncHandler;
+  
