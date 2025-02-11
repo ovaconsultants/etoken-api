71fc92f56368cad35ce528390f22ddb32c630c8f
Praveer Panghal <admin@spinoinc.com>
2025-02-11 17:16:38 -0500

    .

--- controllers/exceptionController.js
+++ controllers/exceptionController.js
@@ -11,7 +11,6 @@ Sample request body:{
 */
 const insertExceptionLog = asyncHandler(async (req, res) => {
   const { exception_description, platform, created_by } = req.body;
-
   // Validate required fields
   if (!exception_description || !platform || !created_by) {
     return res.status(400).json({
@@ -20,52 +19,36 @@ const insertExceptionLog = asyncHandler(async (req, res) => {
       error: "Missing required fields.",
     });
   }
-  try {
-    const result = await db.query(
-      "CALL etoken.sp_insert_exception_log($1, $2, $3, NULL);",
-      [exception_description, platform, created_by]
-    );
-    res.status(201).json({
-      success: true,
-      message: "Exception logged successfully.",
-      exception_id: result.rows[0]?.new_exception_id || null,
-      error: null,
-    });
-
-  } catch (error) {
-    console.error("Error logging exception:", error.message);
-    res.status(500).json({
-      success: false,
-      message: "Internal Server Error",
-      exception_id: null,
-      error: error.message,
-    });
-  }
+  const result = await db.query(
+    "CALL etoken.sp_insert_exception_log($1, $2, $3, NULL);",
+    [exception_description, platform, created_by]
+  );
+  res.status(201).json({
+    success: true,
+    message: "Exception logged successfully.",
+    exception_id: result.rows[0]?.new_exception_id || null,
+    error: null,
+  });
 });
 /*
 URL: GET /api/exception/fetchAllExceptions
 */
 const fetchAllExceptions = asyncHandler(async (req, res) => {
-    try {
-        // Call the function to fetch exceptions
-        const result = await db.query("SELECT * FROM etoken.fn_fetch_all_exceptions();");
-
-        res.status(200).json({
-            success: true,
-            message: result.rows.length > 0 ? "Exceptions retrieved successfully." : "No exceptions found.",
-            exceptions: result.rows || [],
-            error: null
-        });
-
-    } catch (error) {
-        console.error("Error fetching exceptions:", error.message);
-        res.status(500).json({
-            success: false,
-            message: "Internal Server Error",
-            exceptions: [],
-            error: error.message
-        });
-    }
+  const result = await db.query(
+    "SELECT * FROM etoken.fn_fetch_all_exceptions();"
+  );
+  res.status(200).json({
+    success: true,
+    message:
+      result.rows.length > 0
+        ? "Exceptions retrieved successfully."
+        : "No exceptions found.",
+    exceptions: result.rows || [],
+    error: null,
+  });
 });
 
-module.exports = { insertExceptionLog: [protect, insertExceptionLog], fetchAllExceptions };
+module.exports = {
+  insertExceptionLog: [protect, insertExceptionLog],
+  fetchAllExceptions,
+};
--- middlewares/asyncHandler.js
+++ middlewares/asyncHandler.js
@@ -2,12 +2,12 @@ const db = require("../config/db");
 
 const asyncHandler = (fn) => async (req, res, next) => {
     try {
-      console.log("✅ asyncHandler executed for:", req.originalUrl); // Debug log
+     // console.log("✅ asyncHandler executed for:", req.originalUrl); // Debug log
         await fn(req, res, next);
     } catch (error) {
-        console.error("Error:", error.message);
-        const platform = "API";
-        const created_by = "System via asyncHandler "; 
+      //  console.error("Error:", error.message);
+        const platform = "API Server";
+        const created_by = "AsyncHandler "; 
         try {
             await db.query(
                 "CALL etoken.sp_insert_exception_log($1, $2, $3, NULL);",
@@ -16,7 +16,6 @@ const asyncHandler = (fn) => async (req, res, next) => {
         } catch (logError) {
             console.error("Failed to log exception:", logError.message);
         }
-
         // Send error response
         res.status(500).json({
             success: false,
