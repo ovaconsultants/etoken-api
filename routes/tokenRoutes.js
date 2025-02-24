38256dfef357ea89bbc9faeba924e001d43f754b
Praveer Panghal <admin@spinoinc.com>
2025-02-24 12:18:22 -0500

    insertToken

--- routes/tokenRoutes.js
+++ routes/tokenRoutes.js
@@ -1,13 +1,15 @@
 const express = require("express");
-const { insertToken, fetchTokensForPatients } = require("../controllers/tokenController");
+const {
+  insertToken,
+  fetchTokensForPatients,
+} = require("../controllers/tokenController");
 
 //const authMiddleware = require("../middleware/authMiddleware");
 const router = express.Router();
 
 // Public Routes
-router.post("/createToken", insertToken);
+router.post("/insertToken", insertToken);
 router.get("/fetchTokensForPatients", fetchTokensForPatients);
-
 
 //router.use(authMiddleware);
 
